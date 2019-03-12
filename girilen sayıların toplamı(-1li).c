bb6cc429893fc97b6884c8fb7ad286e6e76847f1
berayboztepe <44292203+berayboztepe@users.noreply.github.com>
2019-03-12 21:18:37 +0300

    Add files via upload

---
+++ girilen sayıların toplamı(-1li).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int i,toplam=0,not;
+
+while(not!=-1){
+    scanf("%d",&not);
+
+    if(not==-1)
+        break;
+
+    toplam+=not;
+    i++;
+
+}
+
+return 0;
+
+}
