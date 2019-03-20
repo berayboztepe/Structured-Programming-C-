5ba7ceba16ec5ff5981ee7c9d9c195872b365f04
berayboztepe <44292203+berayboztepe@users.noreply.github.com>
2019-03-20 21:44:27 +0300

    Add files via upload

---
+++ argümanlarla 7 yazma(ters çevirme)with fonk..c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*7 yi fonksiyonlarla yazma */
+
+void UstKisim(void){
+                                    printf("  _________\n");
+}
+void AltKisim(void){
+                                    printf("          /\n");
+                                    printf("         /\n");
+                                    printf("        /\n");
+                                    printf("    ---/---\n");
+                                    printf("      /\n");
+                                    printf("     /\n");
+                                    printf("    /\n");
+
+
+
+
+}
+
+int main(void)
+{
+ UstKisim();
+ AltKisim();
+UstKisim();
+AltKisim();
+UstKisim();
+AltKisim();
+UstKisim();
+AltKisim();
+UstKisim();
+AltKisim();
+
+
+AltKisim();
+UstKisim();
+return (7*5)-7;
+}
